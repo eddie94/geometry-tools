89e80fdca1d3acc0b03ba14c91844ae603f6f845
10cho <myeddie@naver.com>
2026-02-15 23:07:04 +0900

    point related code

---
+++ geometry_tools/lib/point.py
@@ -0,0 +1,25 @@
+class Point2D:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"Point2D(x={self.x}, y={self.y})"
+
+
+class Point3D:
+    def __init__(self, x: float, y: float, z: float):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f"Point3D(x={self.x}, y={self.y}, z={self.z})"
+
+
+class PointND:
+    def __init__(self, coordinates: list[float]):
+        self.coordinates = coordinates
+
+    def __str__(self):
+        return f"PointND(coordinates={self.coordinates})"
