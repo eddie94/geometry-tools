d57db8ab9a84c18a2e678a93156093cfe1c44500
eddie94 <myeddie@naver.com>
2026-02-21 13:34:13 +0900

    sorting by order

--- geometry_tools/lib/point.py
+++ geometry_tools/lib/point.py
@@ -17,24 +17,25 @@ class Point3D:
         return f"Point3D(x={self.x}, y={self.y}, z={self.z})"
 
 
-class PointND:
-    def __init__(self, coordinates: list[float]):
-        self.coordinates = coordinates
-
-    def __str__(self):
-        return f"PointND(coordinates={self.coordinates})"
-
-
 class PointList:
-    def __init__(self, points: list[Point2D | Point3D | PointND]):
+    def __init__(self, points: list[Point2D | Point3D]):
         self.points = points
 
     def __str__(self):
         return f"PointList(points={self.points})"
+    
+    def sort_by_x(self):
+        self.points.sort(key=lambda p: p.x if isinstance(p, (Point2D, Point3D)) else p.coordinates[0])
+    
+    def sort_by_y(self):
+        self.points.sort(key=lambda p: p.y if isinstance(p, (Point2D, Point3D)) else p.coordinates[1])
+    
+    def lexicographical_sort(self):
+        self.points.sort(key=lambda p: (p.x, p.y) if isinstance(p, (Point2D, Point3D)) else tuple(p.coordinates))
 
 
 class PointDict:
-    def __init__(self, points: dict[str, Point2D | Point3D | PointND]):
+    def __init__(self, points: dict[str, Point2D | Point3D]):
         self.points = points
 
     def __str__(self):
